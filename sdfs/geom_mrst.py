46cd6b3df4239b1ce2763d35d229ed6939e79167
Yu-Hong Yeung <yeungyh@gmail.com>
2023-08-24 16:05:52 -0500

    update the changes

--- sdfs/geom_mrst.py
+++ sdfs/geom_mrst.py
@@ -6,20 +6,12 @@ from matplotlib import patches as mpatches
 from matplotlib.collections import PatchCollection
 from matplotlib.axes import Axes
 
-@dataclass
+@dataclass(frozen=True)
 class Nodes:
     num: int
     coords: npt.NDArray[np.float_]
 
-@dataclass
-class Cells:
-    num: int
-    nodes: npt.NDArray[np.int_]
-    centroids: npt.NDArray[np.float_]
-    to_hf: npt.NDArray[np.int_]
-    volumes: npt.NDArray[np.float_]
-
-@dataclass
+@dataclass(frozen=True)
 class Faces:
     num: int
     nodes: npt.NDArray[np.int_]
@@ -31,11 +23,26 @@ class Faces:
     num_interior: int
     int_ext: npt.NDArray[np.int_]
 
+@dataclass(frozen=True)
+class Cells:
+    num: int
+    nodes: npt.NDArray[np.int_]
+    centroids: npt.NDArray[np.float_]
+    to_hf: npt.NDArray[np.int_]
+    volumes: npt.NDArray[np.float_]
+    polygons: npt.NDArray[np.float_]
+
 class GeomMRST:
 
     def __init__(self, filename: str) -> None:
 
         with h5py.File(filename, 'r') as f:
+            nodes_num = int(f.get('nodes/num')[:].item())
+            nodes_coords = f.get('nodes/coords')[:]
+
+            self.nodes = Nodes(nodes_num,
+                               nodes_coords)
+        
             faces_num = int(f.get('faces/num')[:].item())
             faces_neighbors = f.get('faces/neighbors')[:].astype(int) - 1
 
@@ -74,37 +81,34 @@ class GeomMRST:
             cells_volumes = f.get('cells/volumes')[:]
             cells_to_hf = np.concatenate((faces_neighbors[:, :faces_num_interior].ravel(),
                                           faces_neighbors[:, faces_num_interior:].max(axis=0)))
-            
+            cells_polygons = nodes_coords.T[cells_nodes.T]
+
             self.cells = Cells(cells_num,
                                cells_nodes,
                                cells_centroids,
                                cells_to_hf,
-                               cells_volumes)
+                               cells_volumes,
+                               cells_polygons)
 
-            nodes_num = int(f.get('nodes/num')[:].item())
-            nodes_coords = f.get('nodes/coords')[:]
+    def areas(self) -> npt.NDArray[np.float_]:
+        return np.abs(np.sum(self.cells.polygons[..., 0] *
+                             (np.roll(self.cells.polygons[..., 1], 1, 1) -
+                              np.roll(self.cells.polygons[..., 1], -1, 1)), axis=1)) / 2
 
-            self.nodes = Nodes(nodes_num,
-                               nodes_coords)
+    def cellsContain(self, points: npt.NDArray[np.float_]) -> npt.NDArray[np.int_]:
+        return np.nonzero(np.all(np.cross(self.cells.polygons - np.roll(self.cells.polygons, 1, 1),
+                                          points[:, None, None] - self.cells.polygons[None]) > 0, 2))[1]
 
-    def areas(self):
-        polygons = self.nodes.coords.T[self.cells.nodes.T, :]
-        return np.abs(np.sum(polygons[..., 0] * (np.roll(polygons[..., 1], 1, 1) - np.roll(polygons[..., 1], -1, 1)), axis=1)) / 2
-
-    def cellsContain(self, points: npt.NDArray) -> npt.NDArray:
-        polygons = self.nodes.coords.T[self.cells.nodes.T, :]
-        return np.nonzero(np.all(np.cross(polygons - np.roll(polygons, 1, 1), points[:, None, None, :] - polygons[None, ...]) > 0, 2))[1]
-
-    def anyCellsWithin(self, polygons: npt.NDArray) -> npt.NDArray:
+    def anyCellsWithin(self, polygons: npt.NDArray[np.float_]) -> npt.NDArray[np.int_]:
         cells = np.nonzero(np.all(np.cross(polygons - np.roll(polygons, 1, 1),
-                                           self.cells.centroids.T[:, None, None, :] - polygons[None, ...]) > 0, 2))
+                                           self.cells.centroids.T[:, None, None] - polygons[None]) > 0, 2))
         cands = cells[0][np.argsort(cells[1])].reshape((-1, 4))
         return cands[np.arange(len(cands)), np.random.random_integers(0, 3, cands.shape[0])]
 
     
 def geom_plot(geom: GeomMRST, values: npt.ArrayLike, ax: Axes):
 
-    patches = [mpatches.Polygon(v, closed=True) for v in geom.nodes.coords.T[geom.cells.nodes.T, :]]
+    patches = [mpatches.Polygon(v, closed=True) for v in geom.cells.polygons]
     
     p = PatchCollection(patches)
     p.set_array(values)
